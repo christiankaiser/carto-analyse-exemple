886ceaaf1eb66b1810370f880215605ebdbe77fb
Christian Kaiser <ck@naxio.ch>
2017-03-16 17:08:40 +0100

    Étape 3: Fichiers statiques et templates

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 import flask
+from random import choice
 
 app = flask.Flask(__name__)
 app.debug = True
 
 @app.route('/')
 def index():
-    return 'Bonjour!'
+    return flask.render_template(
+        'index.html',
+        random=choice(range(1,46))
+    )
 
 if __name__ == '__main__':
     app.run()
---
+++ static/script.js
@@ -0,0 +1,1 @@
+console.log('Hello, je suis script.js');
